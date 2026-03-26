e8fd0dfbd4ac2390a86a0b47482f69a1afaa84ba
jub <rubikscubescramble@gmail.com>
2026-03-26 21:02:03 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+"name": "Gary",
+  "version": "3.0.0",
+  "description": "24/7 Scotsman",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "mineflayer": "^4.20.0"
+  }
+}
